80eb9dfcda001b86972f2e6fa709440e195a497e
erokemwa <erokemwa@yahoo.com>
2022-11-09 04:18:39 -0800

    Prototype

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+
+#endif /*SIMPLE_SHELL_H*/
